71b59904804240eb022280ea585aef9c88c08516
squir88 <strelcsov@gmail.com>
2015-07-23 23:55:04 +0300

    Изменения: 1. Добавлен D-pad для айпада 2. Пулялки по нажатию на действие 3. Убрана папка node_modules из-за обращения к которой были проблемы с доступом на сервер

--- public/src/game.js
+++ public/src/game.js
@@ -1,93 +1,104 @@
 var Q = Quintus({audioSupported: [ 'wav','mp3' ]})
-      .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio')
-      .setup({ maximize: true })
-      .enableSound()
-      .controls().touch();
+    .include('Sprites, Scenes, Input, 2D, Anim, Touch, UI, Audio')
+    .setup({ maximize: true })
+    .enableSound()
+    //.controls().touch();
+    .controls(true);// 4 ipad
+
+var players = [];
+var socket = io('http://192.168.0.107:8080');
+var UiPlayers = document.getElementById("players");
 
 Q.gravityY = 0;
 
-require(['socket.io/socket.io.js']);
-
-var players = [];
-var socket = io.connect('http://104.167.115.218:8080');
-var UiPlayers = document.getElementById("players");
-var selfId, player;
+var SPRITE_PLAYER = 1;
+var SPRITE_BULLET = 2;
+var SPRITE_ENEMY  = 3;
 
 var objectFiles = [
-  './src/player'
+    './src/player'
 ];
 
 require(objectFiles, function () {
-  function setUp (stage) {
-    socket.on('count', function (data) {
-      UiPlayers.innerHTML = 'Players: ' + data['playerCount'];
+    function setUp (stage) {
+        socket.on('count', function (data) {
+            UiPlayers.innerHTML = 'Players: ' + data['playerCount'];
+        });
+
+        socket.on('connected', function (data) {
+            selfId = data['playerId'];
+            player = new Q.Player({ playerId: selfId, x: 100, y: 100, socket: socket });
+            stage.insert(player);
+            stage.add('viewport').follow(player);
+        });
+
+        socket.on('updated', function (data) {
+            var actor = players.filter(function (obj) {
+                return obj.playerId == data['playerId'];
+            })[0];
+            if (actor) {
+                actor.player.p.x = data['x'];
+                actor.player.p.y = data['y'];
+                actor.player.p.sheet = data['sheet'];
+                actor.player.p.update = true;
+            } else {
+                var temp = new Q.Actor({ playerId: data['playerId'], x: data['x'], y: data['y'], sheet: data['sheet'] });
+                players.push({ player: temp, playerId: data['playerId'] });
+                stage.insert(temp);
+            }
+        });
+    }
+
+    Q.scene('arena', function (stage) {
+        stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
+
+       // var player = stage.insert(new Q.Player({ x: 100, y: 100 }));
+       // stage.add('viewport').follow(player);
+        setUp(stage);
     });
 
-    socket.on('connected', function (data) {
-      selfId = data['playerId'];
-      if (data['tagged']) {
-        player = new Q.Player({ playerId: selfId, x: 100, y: 100, socket: socket });
-        player.p.sheet = 'enemy'
-        player.p.tagged = true;
-        stage.insert(player);
-      } else {
-        player = new Q.Player({ playerId: selfId, x: 100, y: 100, socket: socket });
-        stage.insert(player);
-        player.trigger('join');
-      }
-      stage.add('viewport').follow(player);
-    });
+    var files = [
+        '/images/tiles.png',
+        '/maps/arena.json',
+        '/images/sprites.png',
+        '/images/sprites.json',
+        '/images/bullsprites.png',
+        '/images/bullsprites.json'
+    ];
 
-    socket.on('updated', function (data) {
-      var actor = players.filter(function (obj) {
-        return obj.playerId == data['playerId'];
-      })[0];
-      if (actor) {
-        actor.player.p.x = data['x'];
-        actor.player.p.y = data['y'];
-        actor.player.p.sheet = data['sheet'];
-        actor.player.p.opacity = data['opacity'];
-        actor.player.p.invincible = data['invincible'];
-        actor.player.p.tagged = data['tagged'];
-        actor.player.p.update = true;
-      } else {
-        var temp = new Q.Actor({ playerId: data['playerId'], x: data['x'], y: data['y'], sheet: data['sheet'], opacity: data['opacity'], invincible: data['invincible'], tagged: data['tagged'] });
-        players.push({ player: temp, playerId: data['playerId'] });
-        stage.insert(temp);
-      }
-    });
+    Q.MovingSprite.extend("Bullet", {
+        init: function(p) {
+            this._super(p, {
+                sheet: "bullet",
+                sprite: "bullet",
+                type: SPRITE_BULLET,
+                collisionMask: SPRITE_ENEMY,
+                sensor: true
+            });
 
-    socket.on('tagged', function (data) {
-      if (data['playerId'] == selfId) {
-        player.p.sheet = 'enemy';
-        player.p.tagged = true;
-      } else {
-        var actor = players.filter(function (obj) {
-          return obj.playerId == data['playerId'];
-        })[0];
-        if (actor) {
-          actor.player.p.sheet = 'enemy'
+            /**
+             * Adding 2d component to activate collisions detection and movement.
+             */
+            this.add("2d");
+        },
+
+        /**
+         * I'm overriding step method to destroy bullets.
+         * Every bullet outside the canvas should be destroyed. If I don't destroy
+         * them, the bullet is still alive and can kill enemies outside canvas where
+         * the player cannot see anything.
+         */
+        step: function(dt) {
+            if (this.p.y < 0) {
+                this.destroy();
+            }
         }
-      }
     });
-  }
 
-  Q.scene('arena', function (stage) {
-    stage.collisionLayer(new Q.TileLayer({ dataAsset: '/maps/arena.json', sheet: 'tiles' }));
-
-    setUp(stage);
-  });
-
-  var files = [
-    '/images/tiles.png',
-    '/maps/arena.json',
-    '/images/sprites.png',
-    '/images/sprites.json'
-  ];
-
-  Q.load(files.join(','), function () {
-    Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
-    Q.compileSheets('/images/sprites.png', '/images/sprites.json');
-    Q.stageScene('arena', 0);
-  });
+    Q.load(files.join(','), function () {
+        Q.sheet('tiles', '/images/tiles.png', { tilew: 32, tileh: 32 });
+        Q.compileSheets('/images/sprites.png', '/images/sprites.json');
+        Q.compileSheets('/images/bullsprites.png', '/images/bullsprites.json');
+        Q.stageScene('arena', 0);
+    });
 });
--- public/src/player.js
+++ public/src/player.js
@@ -1,4 +1,34 @@
 require([], function () {
+  Q.Sprite.extend('Player', {
+    init: function (p) {
+      this._super(p, {
+        sheet: 'player'
+      });
+
+      Q.input.on("fire", this, "shoot");
+
+      this.add('2d, platformerControls, animation');
+    },
+    step: function (dt) {
+      if (Q.inputs['up']) {
+        this.p.vy = -200;
+      } else if (Q.inputs['down']) {
+        this.p.vy = 200;
+      } else if (!Q.inputs['down'] && !Q.inputs['up']) {
+        this.p.vy = 0;
+      }
+      this.p.socket.emit('update', { playerId: this.p.playerId, x: this.p.x, y: this.p.y, sheet: this.p.sheet });
+    },
+    shoot: function() {
+      var p = this.p;
+      this.stage.insert(new Q.Bullet({
+        x: p.x,
+        y: p.y - p.w / 2,
+        vy: -200
+      }))
+    }
+  });
+
   Q.Sprite.extend('Actor', {
     init: function (p) {
       this._super(p, {
@@ -12,64 +42,6 @@ require([], function () {
         }
         temp.p.update = false;
       }, 3000);
-    } 
-  });
-
-  Q.Sprite.extend('Player', {
-    init: function (p) {
-      this._super(p, {
-        sheet: 'player',
-        tagged: false,
-        invincible: false,
-        vyMult: 1
-      });
-      this.add('2d, platformerControls, animation');
-
-      this.addEventListeners();
-    },
-    addEventListeners: function () {
-      this.on('hit', function (collision) {
-        if (this.p.tagged && collision.obj.isA('Actor') && !collision.obj.p.tagged && !collision.obj.p.invincible) {
-          this.p.socket.emit('tag', { playerId: collision.obj.p.playerId });
-          this.p.tagged = false;
-          this.p.sheet = 'player';
-          this.p.invincible = true;
-          this.p.opacity = 0.5;
-          this.p.speed = 300;
-          this.p.vyMult = 1.5;
-          var temp = this;
-          setTimeout(function () {
-            temp.p.invincible = false;
-            temp.p.opacity = 1;
-            temp.p.speed = 200;
-            temp.p.vyMult = 1;
-          }, 3000);
-        }
-      });
-
-      this.on('join', function () {
-        this.p.invincible = true;
-        this.p.opacity = 0.5;
-        this.p.speed = 300;
-        this.p.vyMult = 1.5;
-        var temp = this;
-        setTimeout(function () {
-          temp.p.invincible = false;
-          temp.p.opacity = 1;
-          temp.p.speed = 200;
-          temp.p.vyMult = 1;
-        }, 3000);
-      });
-    },
-    step: function (dt) {
-      if (Q.inputs['up']) {
-        this.p.vy = -200 * this.p.vyMult;
-      } else if (Q.inputs['down']) {
-        this.p.vy = 200 * this.p.vyMult;
-      } else if (!Q.inputs['down'] && !Q.inputs['up']) {
-        this.p.vy = 0;
-      }
-      this.p.socket.emit('update', { playerId: this.p.playerId, x: this.p.x, y: this.p.y, sheet: this.p.sheet, opacity: this.p.opacity, invincible: this.p.invincible, tagged: this.p.tagged });
     }
   });
 });
